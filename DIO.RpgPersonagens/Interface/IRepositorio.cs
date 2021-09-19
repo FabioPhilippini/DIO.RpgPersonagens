b71ace597c000d4cbe49db341fcb9c9bc3f6d51b
FabioPhilippini <fabioandre381@gmail.com>
2021-09-19 01:33:20 -0300

    interface e repositório criados

---
+++ DIO.RpgPersonagens/Interface/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIO.RpgPersonagens.Interface
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Inserir(T entidade);
+        void Excluir(int id);
+        void Atualizar(int id, T entidade);
+        int ProximoId();
+    }
+}
---
+++ DIO.RpgPersonagens/Repositorio/PersonagemRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using DIO.RpgPersonagens.Entidade;
+using DIO.RpgPersonagens.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace DIO.RpgPersonagens.Repositorio
+{
+    public class PersonagemRepositorio : IRepositorio<Personagem>
+    {
+
+        private List<Personagem> listaPersonagens = new List<Personagem>();
+
+        public void Atualizar(int id, Personagem personagem)
+        {
+            listaPersonagens[id] = personagem;
+        }
+
+        public void Excluir(int id)
+        {
+            listaPersonagens[id].Remover();
+        }
+
+        public void Inserir(Personagem personagem)
+        {
+            listaPersonagens.Add(personagem);
+        }
+
+        public List<Personagem> Lista()
+        {
+            return listaPersonagens;
+        }
+
+        public int ProximoId()
+        {
+            return listaPersonagens.Count;
+        }
+
+        public Personagem RetornaPorId(int id)
+        {
+            return listaPersonagens[id];
+        }
+    }
+}
